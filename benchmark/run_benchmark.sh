48db2c4656285fc9fa68307b51b2dcda7b7babfd
savojard <castrense.savojardo2@unibo.it>
2021-04-06 11:07:44 +0200

    Add benchmarking code

--- benchmark/run_benchmark.sh
+++ benchmark/run_benchmark.sh
@@ -15,10 +15,10 @@ for pdbid in $(cat ${bts_list}); do
   hhm_tmp_file=$(mktemp --tmpdir=${tmpworkdir} --suffix=.hhm)
   out_tmp_file=$(mktemp --tmpdir=${tmpworkdir} --suffix=.tsv)
   rsa_tmp_file=$(mktemp --tmpdir=${tmpworkdir} --suffix=.rsa)
-  gunzip -c blin_test_set/fasta/${pdbid}.fasta.gz > ${fas_tmp_file}
-  gunzip -c blin_test_set/aln/${pdbid}.a3m.gz > ${a3m_tmp_file}
-  gunzip -c blin_test_set/aln/${pdbid}.hhm.gz > ${hhm_tmp_file}
-  gunzip -c blin_test_set/rsa/${pdbid}.rsa.gz > ${rsa_tmp_file}
+  gunzip -c blind_test_set/fasta/${pdbid}.fasta.gz > ${fas_tmp_file}
+  gunzip -c blind_test_set/aln/${pdbid}.a3m.gz > ${a3m_tmp_file}
+  gunzip -c blind_test_set/aln/${pdbid}.hhm.gz > ${hhm_tmp_file}
+  gunzip -c blind_test_set/rsa/${pdbid}.rsa.gz > ${rsa_tmp_file}
   deeprex.py aln -f ${fas_tmp_file} -a ${a3m_tmp_file} -m ${hhm_tmp_file} -o ${out_tmp_file}
   paste ${out_tmp_file} ${rsa_tmp_file} | cut -f 1-7,12,13 >> ${output_file}
   rm ${fas_tmp_file} ${a3m_tmp_file} ${hhm_tmp_file} ${out_tmp_file} ${rsa_tmp_file}
